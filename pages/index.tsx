e6d6af84a3aaf8fe85d4bf8384c9e4044fb1c37b
royappeldoorn <roy.appeldoorn@gmail.com>
2022-06-19 12:54:02 +0200

    Show random quote on page refresh

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,49 @@
 import type { NextPage } from "next";
+import { useQuery, useQueryClient } from "react-query";
+import Quote from "../components/Quote";
+
+interface Quote {
+  _id: string;
+  content: string;
+  author: string;
+  authorSlug: string;
+  length: number;
+  tags: String[];
+}
 
 const Home: NextPage = () => {
+  const {
+    isLoading,
+    error,
+    data: quote,
+    isError,
+  } = useQuery<Quote, Error>("quoteData", () =>
+    fetch("https://api.quotable.io/random").then((res) => res.json())
+  );
+
+  const queryClient = useQueryClient();
+
+  const refetch = () => {
+    queryClient.invalidateQueries("quoteData");
+  };
+
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      {quote ? <Quote content={quote.content} author={quote.author} /> : null}
+      <button
+        className="px-4 py-2 text-gray-700 bg-white rounded-sm"
+        onClick={refetch}
+      >
+        Renew!
+      </button>
     </div>
   );
 };
