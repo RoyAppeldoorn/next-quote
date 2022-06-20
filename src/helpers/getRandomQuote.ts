6476fee1930ce69b268e10521781fe174ad3caa6
royappeldoorn <roy.appeldoorn@gmail.com>
2022-06-20 12:31:39 +0200

    Added helper function for always showing a unique quote

---
+++ src/helpers/getRandomQuote.ts
@@ -0,0 +1,18 @@
+// we currently now that there are 44 quotes in de database
+const MAX_QUOTE_ID = 44;
+
+export const getUniqueQuoteId: (notThisOne?: number) => number = (
+  notThisOne
+) => {
+  const quoteId = Math.floor(Math.random() * MAX_QUOTE_ID) + 1;
+
+  if (quoteId !== notThisOne) return quoteId;
+  return getUniqueQuoteId(notThisOne);
+};
+
+export const getVotingOptions = () => {
+  const firstId = getUniqueQuoteId();
+  const secondId = getUniqueQuoteId(firstId);
+
+  return [firstId, secondId];
+};
