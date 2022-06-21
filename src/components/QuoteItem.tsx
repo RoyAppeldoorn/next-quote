43f746c1662758a2e1f9ec7062bc4b29ba3955bd
Roy-Appeldoorn <roy.appeldoorn@amplexor.com>
2022-06-21 16:50:49 +0200

    Added voting button

--- src/components/QuoteItem.tsx
+++ src/components/QuoteItem.tsx
@@ -7,17 +7,30 @@ export type Quote = {
 
 interface QuoteItemProps {
   quote: Quote;
+  vote: () => void;
 }
 
-const QuoteItem = ({ quote }: QuoteItemProps) => (
-  <blockquote className="relative max-w-xl p-4 m-8 text-2xl italic border-l-4 bg-neutral-100 text-neutral-600 border-neutral-500">
-    <p className="mb-4">&quot;{quote.quote}&quot;</p>
-    <cite className="flex items-center">
-      <div className="flex flex-col items-start">
-        <span className="mb-1 text-sm italic font-bold">{quote.author}</span>
-      </div>
-    </cite>
-  </blockquote>
+const btn =
+  "items-center px-3 py-1.5 border border-gray-300 shadow-sm font-medium rounded-full text-gray-700 bg-white hover:bg-gray-100";
+
+const QuoteItem = ({ quote, vote }: QuoteItemProps) => (
+  <div className="flex flex-col items-center">
+    <blockquote
+      key={quote.id}
+      className="relative max-w-xl pb-4 m-8 text-lg sm:text-xl italic border-b-2 border-gray-700"
+    >
+      <p className="mb-4">&quot;{quote.quote}&quot;</p>
+      <cite className="flex items-center">
+        <div className="flex flex-col items-start">
+          <span className="mb-1 text-sm italic font-bold">{quote.author}</span>
+        </div>
+      </cite>
+    </blockquote>
+
+    <button className={btn} onClick={() => vote()}>
+      Funnier
+    </button>
+  </div>
 );
 
 export default QuoteItem;
