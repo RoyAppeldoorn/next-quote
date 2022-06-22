99aacc0c9bcce49a83309f64e93d78ef4b88e3e4
royappeldoorn <roy.appeldoorn@gmail.com>
2022-06-22 11:25:22 +0200

    Active nav link styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,38 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
-const Navbar = () => (
-  <nav>
-    <div className="mb-8 text-4xl font-bold text-center">
-      Which quote is funnier?
-    </div>
-    <div className="w-full text-xl text-center">
-      <Link href="/">
-        <a className="hover:text-gray-400">Home</a>
-      </Link>
-      <span className="p-4">{"-"}</span>
-      <Link href="/results">
-        <a className="hover:text-gray-400">Results</a>
-      </Link>
-      <a href="https://github.com/RoyAppeldoorn/next-quote" />
-    </div>
-  </nav>
-);
+export const Navbar = () => {
+  const router = useRouter();
+
+  return (
+    <nav>
+      <div className="mb-8 text-4xl font-bold text-center">
+        Which quote is funnier?
+      </div>
+      <div className="w-full text-xl text-center">
+        <Link href="/">
+          <a
+            className={`${
+              router.pathname === "/" ? "text-white" : "text-gray-400"
+            }`}
+          >
+            Home
+          </a>
+        </Link>
+        <span className="p-4">{"-"}</span>
+        <Link href="/results">
+          <a
+            className={`${
+              router.pathname === "/results" ? "text-white" : "text-gray-400"
+            }`}
+          >
+            Results
+          </a>
+        </Link>
+        <a href="https://github.com/RoyAppeldoorn/next-quote" />
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
